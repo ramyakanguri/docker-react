260a7cd8f1bf86455c1b6a554503c54a9f6d7a7e
ramyakanguri <kanguri.r@northeastern.edu>
2024-05-11 17:18:53 -0400

    app.js changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          byeee
+          I was changed
         </p>
         <a
           className="App-link"
